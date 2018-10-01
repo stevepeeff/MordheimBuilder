c85defdbaed50b305cce7c9d2108b1f29ae3e54c
Stefan Matthijssen <ziggasoftware@gmail.com>
2018-10-01 06:45:26 +0200

    Added Vampire

---
+++ DomainModel/Warbands/BaseClasses/UndeadHeroBase.cs
@@ -0,0 +1,26 @@
+﻿using DomainModel.Equipment;
+using DomainModel.Equipment.Armour;
+using DomainModel.Equipment.Weapons.CloseCombat;
+using DomainModel.Equipment.Weapons.Missile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.Warbands.BaseClasses
+{
+    internal abstract class UndeadHeroBase : HeroBase, IHero
+    {
+        public UndeadHeroBase()
+        {
+        }
+
+        static protected List<IEquipment> HeroEquipmentList { get; } = new List<IEquipment>()
+        {
+            new Dagger(),  new ClubMaceHammer(), new Axe(),  new Sword(),  new DoubleHandedWeapon(),new Spear(), new Halberd(),
+            new Bow(), new ShortBow(),
+            new LightArmour(),  new HeavyArmor(),   new Shield(), new Helmet(),
+        };
+    }
+}
---
+++ DomainModel/Warbands/Undead/Vampire.cs
@@ -0,0 +1,40 @@
+﻿using DomainModel.Skills;
+using DomainModel.Warbands.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.Warbands.Undead
+{
+    internal class Vampire : UndeadHeroBase
+    {
+        public Vampire()
+        {
+            Movement.BaseValue = 5;
+            WeaponSkill.BaseValue = 4;
+            BallisticSkill.BaseValue = 4;
+            Strength.BaseValue = 4;
+            Toughness.BaseValue = 4;
+            Wounds.BaseValue = 2;
+            Initiative.BaseValue = 5;
+            Attacks.BaseValue = 2;
+            LeaderShip.BaseValue = 8;
+
+            _AllowedSkills.AddRange(SkillProvider.Instance.CombatSkills);
+            _AllowedSkills.AddRange(SkillProvider.Instance.AcademicSkills);
+            _AllowedSkills.AddRange(SkillProvider.Instance.StrengthSkills);
+            _AllowedSkills.AddRange(SkillProvider.Instance.SpeedSkills);
+        }
+
+        public override int HireFee => 110;
+
+        public override int MaximumAllowedInWarBand => 1;
+
+        public override IWarrior GetANewInstance()
+        {
+            return new Vampire();
+        }
+    }
+}
