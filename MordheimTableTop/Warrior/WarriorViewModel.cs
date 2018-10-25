76faf954ddcda1db1954eba876e6d87b9cd5e001
Stefan Matthijssen <s.matthijssen@zigga.nl>
2018-10-25 13:46:50 +0200

    Setting up Warrior VM

---
+++ MordheimTableTop/Warrior/WarriorViewModel.cs
@@ -0,0 +1,31 @@
+﻿using DomainModel.Warbands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MordheimTableTop.Warrior
+{
+    internal class WarriorViewModel : ViewModelBase
+    {
+        private readonly IWarrior _Warrior;
+
+        public WarriorViewModel(IWarrior warrior)
+        {
+            _Warrior = warrior;
+        }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        public StatisticsViewModel StatisticsVM { get { return new StatisticsViewModel(_Warrior); } }
+
+        public string WarriorTypeName { get { return _Warrior.TypeName.SplitCamelCasing(); } }
+    }
+}
