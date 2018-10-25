1e8bac1fd4db7a55a010b058d5b3d11f55daeb21
Stefan Matthijssen <s.matthijssen@zigga.nl>
2018-10-25 13:09:49 +0200

    Building Exp CheckBox

---
+++ MordheimTableTop/Warrior/ExperienceCheckBox.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MordheimTableTop.Warrior
+{
+    /// <summary>
+    /// Interaction logic for ExperienceCheckBox.xaml
+    /// </summary>
+    public partial class ExperienceCheckBox : CheckBox
+    {
+        public ExperienceCheckBox()
+        {
+            InitializeComponent();
+            BorderThickness = new Thickness(1.0);
+        }
+
+        public int Index { get; }
+
+        public ExperienceCheckBox(int index, bool thickBorder) : this()
+        {
+            Index = index;
+
+            if (thickBorder)
+            {
+                BorderThickness = new Thickness(2.5);
+            }
+        }
+    }
+}
---
+++ MordheimTableTop/Warrior/ExperienceViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DomainModel.Warbands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MordheimTableTop.Warrior
+{
+    internal class ExperienceViewModel : ViewModelBase
+    {
+        public ExperienceViewModel(IWarrior warrior)
+        {
+        }
+    }
+}
